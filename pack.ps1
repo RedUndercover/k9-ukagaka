9ceff48f434e5ff336566def6b507377d2574e41
RedUndercover <redundercover@outlook.com>
2019-08-10 16:22:40 -0400

    Create pack.ps1
    Made Packager for easy .nar creation

---
+++ pack.ps1
@@ -0,0 +1,18 @@
+<#
+K-9 Ghost Packager For PowerShell v0.1
+--------------------------------------
+Purpose: Packages the specified folder as a ghost .nar file for easy installation.
+Authors: RedUndercover, RowanWitch
+
+Uses $$PSScriptRoot, Needs PowerShell V.3+
+Has some problems if you keep old .nar files in the same folder, might be fixed in a future version.
+#>
+
+#Config
+$Folder = "r_k9\" #Folder name w/ ghost files
+$OutputFile = 'K9_Ghost' #Desired output file name
+
+#Scripting
+$GhostPath = Join-Path $PSScriptRoot $Folder
+Compress-Archive -Path $GhostPath'*' -CompressionLevel Fastest  -DestinationPath $PSScriptRoot'\'$OutputFile
+Rename-Item -Path $PSScriptRoot'\'$OutputFile'.zip' -NewName $OutputFile'.nar'

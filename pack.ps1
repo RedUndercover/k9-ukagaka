2b469f8a1d804484b0dacaea5e5e73d4d27d7345
RedUndercover <redundercover@outlook.com>
2019-08-13 03:30:39 -0400

    Partially implemented new cal system, added new library file for data

--- pack.ps1
+++ pack.ps1
@@ -1,5 +1,5 @@
 <#
-K-9 Ghost Packager For PowerShell v0.1
+Ghost Packager For PowerShell v0.1
 --------------------------------------
 Purpose: Packages the specified folder as a ghost .nar file for easy installation.
 Authors: RedUndercover, RowanWitch
@@ -10,7 +10,7 @@ Has some problems if you keep old .nar files in the same folder, might be fixed 
 
 #Config
 $Folder = "r_k9\" #Folder name w/ ghost files
-$OutputFile = 'K9_Ghost' #Desired output file name
+$OutputFile = 'K9_Ghost' #Desired output file name w/o extension
 
 #Scripting
 $GhostPath = Join-Path $PSScriptRoot $Folder
